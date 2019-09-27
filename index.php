8d5b4f44ad1fa44696919eea2c8f7057d5cc1449
Daniel Miguel <daniel-solla@hotmail.com>
2019-09-27 12:09:50 -0300

    inicializando PHP

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    $nomeSistema = "Cursos Online"
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Loja Virtual</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<header class= "d-flex justify-content-between aling-items-center p-3">
+    <h1 id="logo">
+        <?php echo $nomeSistema ?>
+    </h3>
+        <nav>
+            <ul class= "nav" >
+                <li class= "nav-item" >Cursos</li>
+                <li class= "nav-item">Login</li>
+                <li class= "nav-item">Contato</li>
+            </ul>
+        </nav>
+</header>   
+</body>
+</html>
